cbd6e9aa2231914d46154bed47db24b0264cbbb6
OTAVIO FERNANDES DE OLIVEIRA <2021321049@IFAL-MACEIO.INTRANET>
2023-09-12 14:02:53 -0300

    secao3

---
+++ SQL/ORACLE ACADEMY/SEÇÃO 3/aula3.sql
@@ -0,0 +1,68 @@
+/* Introdução a Funções */
+
+/* Funções são pequenos programas que executam uma ação em um valor ou uma coluna e produzem algo diferente como saída*/
+
+/* O Oracle tem dois tipos distintos de funções:
+
+* De Linha única
+* Multilinha
+
+-> Funções de linha única ->
+
+->
+-> Funções multilinha
+->
+
+* As funções de linha única operam somente em linhas únicas e retornam um resultado por linha
+
+* Existem tipos diferentes de funções de linha única, incluindo funções de caractere, número, data e conversão.
+
+* As funções multilinha podem manipular grupos de linhas para fornecer um resultado por grupo de linhas
+
+* Elas também são conhecidas como funções de grupo
+
+- Em SQL, as funções de Linha Única podem ser usadas para:
+
+* Fazer cálculos, como arredondar números para uma casa decimal específica.
+
+* Modificar itens de dados individuais, como converter valores de caractere de letra maiúscula para minúscula.
+
+* Formatar datas e números para exibição, como converter o formato de data interno de um banco de dados número para o padrão.
+
+* Converter os tipos de dados de uma coluna, como uma string de caracteres em um número ou uma data.
+
+As Funções de Linha única aceitam um ou mais argumentos e retornam um único resultado por Linha
+
+Portanto, se você aplicar a função de linha única a 12 linhas, obterá 12 resultados
+
+- Basicamente, as funções de linha única fazem o seguinte:
+
+* Manipulam itens de dados
+
+* Aceitam argumentos e retornam um valor
+
+* Agem em cada linha retornada
+
+* Retornam um resultado por linha 
+
+* Podem modificar o tipo de dados
+
+* Podem ser aninhadas
+
+Funções Multilinha
+
+* As funções Multilinha (ou de Grupo) aceitam muitas linhas como entrada e retornam um único valor como saída
+
+* A entrada de linhas ser a tabela inteira ou dividida em grupos menores
+
+- Exemplos de funções Multilinha (de Grupo) incluem:
+
+* MAX: encontra o maior valor em um grupo de linhas 
+
+* MIN: encontra o menor valor em um grupo de linhas 
+
+* AVG: encontra o valor médio em um grupo de linhas
+
+
+
+ */
---
+++ SQL/ORACLE ACADEMY/SEÇÃO 3/aula3exe.sql
@@ -0,0 +1,28 @@
+/* Tente/solucione
+
+1. Para cada tarefa, indique se uma função de uma única ou de várias linhas seria mais adequada:
+
+a. Exibir todos os endereços de e-mail em letras maiúsculas
+
+b. Determinar o salário médio dos funcionários do departamento de vendas
+
+c. Mostrar as datas de contratação com o mês escrito por extenso (September 1, 2004)
+
+d. Localizar os funcionários mais antigos (com data de contratação mais antiga) de cada
+departamento
+
+e. Exibir os salários dos funcionários arredondados para centena
+
+f. Substituir valores nulos por zero ao exibir as comissões dos funcionários.
+
+2. As funções de várias linhas mais comuns são: AVG, COUNT, MAX, MIN e SUM. Dê a sua própria
+definição para cada uma dessas funções.
+
+3. Teste suas definições substituindo cada uma das funções de várias linhas nesta consulta.
+
+SELECT FUNCTION(salary)
+FROM employees
+
+Escreva cada consulta e seus resultados. 
+
+*/
